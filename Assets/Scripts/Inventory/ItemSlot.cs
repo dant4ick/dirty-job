c04d88ea8902af0f4b23ba3a97024d46b792f194
amorfni4 <maks.sarychev.2003@mail.ru>
2022-11-17 15:02:20 +0300

    Add: swaping weapons in inventory

--- Assets/Scripts/Inventory/ItemSlot.cs
+++ Assets/Scripts/Inventory/ItemSlot.cs
@@ -12,33 +12,53 @@ public class ItemSlot : MonoBehaviour, IDropHandler
 
         if (transform.childCount < 2)
         {
-            Item.Item itemInPrevCell = null;
+            Transform prevCell = eventData.pointerDrag.transform.parent;
+            int idOfPrevCell = FindIdOfChild(prevCell);
+            Item.Item itemInPrevCell = transform.GetComponentInParent<DisplayInventoy>().inventory.ItemList[idOfPrevCell].GetItem();
 
-            for (int child = 0; child < transform.parent.childCount; child++)
-            {
-                if (eventData.pointerDrag.transform.parent.parent.GetChild(child) == eventData.pointerDrag.transform.parent)
-                {
-                    itemInPrevCell = transform.GetComponentInParent<DisplayInventoy>().inventory.ItemList[child].GetItem();
-                    transform.GetComponentInParent<DisplayInventoy>().inventory.SetItem(null, child);
-                    break;
-                }
-            }
-            //eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            transform.GetComponentInParent<DisplayInventoy>().inventory.SetItem(null, idOfPrevCell);
+
+            int idOfCurCell = FindIdOfChild(transform);
+            transform.GetComponentInParent<DisplayInventoy>().inventory.SetItem(itemInPrevCell, idOfCurCell);
+
             eventData.pointerDrag.GetComponent<RectTransform>().transform.SetParent(transform, false);
             eventData.pointerDrag.GetComponent<RectTransform>().position = GetComponent<RectTransform>().position;
-
-            for (int child = 0; child < transform.parent.childCount; child++)
-            {
-                if (transform.parent.GetChild(child) == transform)
-                {
-                    transform.GetComponentInParent<DisplayInventoy>().inventory.SetItem(itemInPrevCell, child);
-                    break;
-                }
-            }
         }
         else
         {
-            //swap weapons
+            Transform prevCell = eventData.pointerDrag.transform.parent;
+            int idOfPrevCell = FindIdOfChild(prevCell);
+            Item.Item itemInPrevCell = transform.GetComponentInParent<DisplayInventoy>().inventory.ItemList[idOfPrevCell].GetItem();
+
+            Transform curCell = transform;
+            int idOfCurCell = FindIdOfChild(curCell);
+            Item.Item itemInCurCell = transform.GetComponentInParent<DisplayInventoy>().inventory.ItemList[idOfCurCell].GetItem();
+
+            transform.GetComponentInParent<DisplayInventoy>().inventory.SetItem(itemInCurCell, idOfPrevCell);
+            transform.GetComponentInParent<DisplayInventoy>().inventory.SetItem(itemInPrevCell, idOfCurCell);
+
+            RectTransform childOfCurCell = curCell.GetChild(1).GetComponent<RectTransform>();
+            RectTransform childOfPrevCell = eventData.pointerDrag.GetComponent<RectTransform>();
+
+            childOfCurCell.transform.SetParent(childOfPrevCell.parent, false);
+            childOfCurCell.position = childOfPrevCell.parent.GetComponent<RectTransform>().position;
+
+            childOfPrevCell.transform.SetParent(transform, false);
+            childOfPrevCell.position = GetComponent<RectTransform>().position;
+
+
         }
+    }
+
+    private int FindIdOfChild(Transform childToFindId)
+    {
+        for (int child = 0; child < transform.parent.childCount; child++)
+        {
+            if (childToFindId.parent.GetChild(child) == childToFindId)
+            {
+                return child;
+            }
+        }
+        return 0;
     }
 }
