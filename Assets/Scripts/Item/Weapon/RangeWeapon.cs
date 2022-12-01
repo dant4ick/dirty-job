42de5fd66396d1a94f4d7b1ee1173228b632e1f1
vrnnncl <97320132+vrnnncl@users.noreply.github.com>
2022-12-01 23:24:01 +0300

    fix: missed a }

--- Assets/Scripts/Item/Weapon/RangeWeapon.cs
+++ Assets/Scripts/Item/Weapon/RangeWeapon.cs
@@ -7,22 +7,53 @@ using UnityEngine;
 public class RangeWeapon : Weapon
 {
     [SerializeField] private AudioClip _soundOnReload;
+
     #region BASIC RANGE WEAPON STATS
-        [Header ("Basic Range Weapon Stats")] 
-        [SerializeField] private float spreadDegrees;
-        [SerializeField] private float distance;
-        [SerializeField] private float penetration;
-        [SerializeField] private int numberOfBulletsPerShot;
-        [SerializeField] private int maxAmmo;
-        [SerializeField] private float reloadTime;
+
+    [Header("Basic Range Weapon Stats")] [SerializeField]
+    private float spreadDegrees;
+
+    [SerializeField] private float distance;
+    [SerializeField] private float penetration;
+    [SerializeField] private int numberOfBulletsPerShot;
+    [SerializeField] private int maxAmmo;
+    [SerializeField] private float reloadTime;
+
     #endregion
 
-    public AudioClip SoundOnReload { get { return _soundOnReload; } }
-    public float SpreadDegrees { get { return spreadDegrees; } }
-    public float Distance { get { return distance; } }
-    public float Penetration { get { return penetration; } }
-    public int NumberOfBulletsPerShot { get { return numberOfBulletsPerShot; } }
+    public AudioClip SoundOnReload
+    {
+        get { return _soundOnReload; }
+    }
 
-    public int MaxAmmo { get { return maxAmmo; } }
-    public float ReloadTime { get { return reloadTime; } }
+    public float SpreadDegrees
+    {
+        get { return spreadDegrees; }
+    }
+
+    public float Distance
+    {
+        get { return distance; }
+    }
+
+    public float Penetration
+    {
+        get { return penetration; }
+    }
+
+    public int NumberOfBulletsPerShot
+    {
+        get { return numberOfBulletsPerShot; }
+    }
+
+    public int MaxAmmo
+    {
+        get { return maxAmmo; }
+    }
+
+    public float ReloadTime
+    {
+        get { return reloadTime; }
+    }
+}
 
